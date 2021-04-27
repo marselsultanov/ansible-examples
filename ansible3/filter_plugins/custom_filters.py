f15dd25191f2233d81765bfa5664839f3d236ca5
Marsel Sultanov <80184639+marselsultanov@users.noreply.github.com>
2021-04-27 17:55:15 +0300

    Add files via upload

---
+++ ansible3/filter_plugins/custom_filters.py
@@ -0,0 +1,22 @@
+def get_mongo_src(arg, os_name, os_version, mongo_version):
+    import re
+
+    os_name = os_name.lower()
+    if os_name == "centos":
+        os_name = "rhel"
+
+    if os_name == "redhat":
+        os_name = "rhel"
+
+    for item in arg:
+        if re.match('.*' + os_name + os_version + '.*', item, re.M|re.I) and re.match('.*' + mongo_version + '.*', item, re.M|re.I):
+            return item
+
+    return "nothing found"
+
+
+class FilterModule(object):
+    def filters(self):
+        return {
+            'get_mongo_src': get_mongo_src
+        }
